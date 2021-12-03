593f321173a1332fdf6279fd279737ee6e6a73ec
Allencaidota <56335140+Allencaidota@users.noreply.github.com>
2021-12-03 13:54:12 -0800

    456

--- src/main/java/com/allen/cmsshoppingcart/controllers/AdminPagesController.java
+++ src/main/java/com/allen/cmsshoppingcart/controllers/AdminPagesController.java
@@ -8,6 +8,7 @@ import com.allen.cmsshoppingcart.models.data.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,6 +29,14 @@ public class AdminPagesController {
         model.addAttribute("pages", pages);
 
         return "admin/pages/index";
+    }
+
+    @GetMapping("/add")
+    public String add(@ModelAttribute Page model) {
+
+        // model.addAttribute("pages", new Page());
+
+        return "admin/pages/add";
     }
 
 }
