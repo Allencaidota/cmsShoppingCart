557b8e7d1b8514cb3bca268fb3acef256fec8f9a
Allencaidota <56335140+Allencaidota@users.noreply.github.com>
2021-12-07 19:54:42 -0800

    update

--- src/main/java/com/allen/cmsshoppingcart/controllers/AdminPagesController.java
+++ src/main/java/com/allen/cmsshoppingcart/controllers/AdminPagesController.java
@@ -2,15 +2,21 @@ package com.allen.cmsshoppingcart.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.allen.cmsshoppingcart.models.PageRepository;
 import com.allen.cmsshoppingcart.models.data.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/pages")
@@ -39,6 +45,47 @@ public class AdminPagesController {
         // model.addAttribute("pages", new Page());
 
         return "admin/pages/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/add";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-")
+                : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepo.findBySlug(slug);
+
+        if (slugExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+
+        } else {
+            page.setSlug(slug);
+            page.setSorting(100);
+
+            pageRepo.save(page);
+        }
+
+        return "redirect:/admin/pages/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Page page = pageRepo.getById(id);
+
+        model.addAttribute("page", page);
+
+        return "admin/pages/edit";
     }
 
 }
--- src/main/java/com/allen/cmsshoppingcart/models/data/Page.java
+++ src/main/java/com/allen/cmsshoppingcart/models/data/Page.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -17,10 +18,12 @@ public class Page {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Size(min = 2, message = "Title must be at least 2 characters long")
     private String title;
 
     private String slug;
 
+    @Size(min = 5, message = "Content must be at least 5 characters")
     private String content;
 
     private int sorting;
