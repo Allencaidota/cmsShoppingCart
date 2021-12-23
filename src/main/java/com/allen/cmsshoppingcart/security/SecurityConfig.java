9d9ec3bfcf68d33eceedcc38487d83da0f4dafed
Allencaidota <56335140+Allencaidota@users.noreply.github.com>
2021-12-23 14:55:23 -0800

    securityconfig

---
+++ src/main/java/com/allen/cmsshoppingcart/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.allen.cmsshoppingcart.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+}
